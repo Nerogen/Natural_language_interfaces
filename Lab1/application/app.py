d44452f4ee2442b246790964228293a9ea4a165c
Nero <14k21096@gmail.com>
2023-01-31 13:38:09 +0300

    update Lab1/application/app.py

--- Lab1/application/app.py
+++ Lab1/application/app.py
@@ -1,2 +1,20 @@
-"""TODO: application"""
+import customtkinter
 
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("green")
+width = 300
+height = 150
+root = customtkinter.CTk()
+root.title("Vocabulary")
+root.geometry(str(width) + "x" + str(height) + "+300+300")
+
+
+def print_data():
+    print(input_data.get())
+
+
+input_data = customtkinter.CTkEntry(root, placeholder_text="input text")
+input_data.pack()
+button = customtkinter.CTkButton(root, text="Update", command=print_data).pack()
+
+root.mainloop()
