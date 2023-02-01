fd1522a57cc7c9102ca194635f60e0375c40d7d1
АД <degterevastasa@gmail.com>
2023-02-01 12:14:43 +0300

    added frame raising (to move between pages)

--- Lab1/application/app.py
+++ Lab1/application/app.py
@@ -4,16 +4,20 @@ def raise_frame(frame):
 
 # to do: read and load data from pdf
 def download_data(file_from, *args, **kwargs):
+    # use frame as kwarg to move from one page to another
+    raise_frame(kwargs['frame'])
     pass
 
 
 # to do: save data in file
 def upload_data(file_to, data, *args, **kwargs):
+    raise_frame(kwargs['frame'])
     pass
 
 
 # to do: display vocabulary records on the screen
 def show_records(vocabulary, *args, **kwargs):
+    raise_frame(kwargs['frame'])
     pass
 
 
@@ -39,6 +43,7 @@ def search_records(*args, **kwargs):
 
 # to do: make new word form by given rules
 def generate_lexeme_form(base_lexeme, *args, **kwargs):
+    raise_frame(kwargs['frame'])
     pass
 
 
