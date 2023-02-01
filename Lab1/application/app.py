ad0e29273603f5292fef4e42a8b3c9230a61a76c
АД <degterevastasa@gmail.com>
2023-02-01 09:22:19 +0300

    added main methods

--- Lab1/application/app.py
+++ Lab1/application/app.py
@@ -13,8 +13,49 @@ def print_data():
     print(input_data.get())
 
 
-input_data = customtkinter.CTkEntry(root, placeholder_text="input text")
-input_data.pack()
-button = customtkinter.CTkButton(root, text="Update", command=print_data).pack()
+# to do: read and load data from pdf
+def download_data(file_from, *args, **kwargs):
+    pass
 
-root.mainloop()
+
+# to do: save data in file
+def upload_data(file_to, data, *args, **kwargs):
+    pass
+
+
+# to do: display vocabulary records on the screen
+def show_records(vocabulary, *args, **kwargs):
+    pass
+
+
+# to do: edit and save existing record
+def edit_record(old_record, *args, **kwargs):
+    pass
+
+
+# to do: save edited record
+def save_record(record, *args, **kwargs):
+    pass
+
+
+# to do: filter records by parameters
+def filter_records(*args, **kwargs):
+    pass
+
+
+# to do: search records by parameters
+def search_records(*args, **kwargs):
+    pass
+
+
+# to do: make new word form by given rules
+def generate_lexeme_form(base_lexeme, *args, **kwargs):
+    pass
+
+
+if __name__ == '__main__':
+    input_data = customtkinter.CTkEntry(root, placeholder_text="input text")
+    input_data.pack()
+    button = customtkinter.CTkButton(root, text="Update", command=print_data).pack()
+
+    root.mainloop()
